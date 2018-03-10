21b0aa7e574f018c331260b467b28997010d9e7d
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-10 14:57:36 +1100

    Fix outstanding code style concerns
    Remove or Fix any lines longer than 80 characters and add an additional
    space between import parameters in order to conform to the `flask8`
    community style guidelines.

--- tests/toy_robot_cli_test.py
+++ tests/toy_robot_cli_test.py
@@ -1,12 +1,11 @@
 import unittest
 
 import toy_robot
-from .test_helpers import InputBuffer,OutputBuffer
+from .test_helpers import InputBuffer, OutputBuffer
 
 
 class ToyRobotCLITestSuite(unittest.TestCase):
     def test_main(self):
-        # with patch('builtins.input', return_value='EXIT\n'), OutputBuffer() as bf:
         with InputBuffer('EXIT\n'), OutputBuffer() as bf:
             self.assertIsNone(toy_robot.main())
         self.assertEqual(bf.out, '')
@@ -32,7 +31,8 @@ class ToyRobotCLITestSuite(unittest.TestCase):
         self.assertEqual(bf.out, '0, 1, NORTH\n')
 
     def test_main_place(self):
-        with InputBuffer('PLACE 3,4,SOUTH\nREPORT\nEXIT\n'), OutputBuffer() as bf:
+        input_text = 'PLACE 3,4,SOUTH\nREPORT\nEXIT\n'
+        with InputBuffer(input_text), OutputBuffer() as bf:
             self.assertIsNone(toy_robot.main())
         self.assertEqual(bf.out, '3, 4, SOUTH\n')
 
